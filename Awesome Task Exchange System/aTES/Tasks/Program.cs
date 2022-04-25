36ae965fe6ca7bc93cf48cc64a3bb6541e3e9416
Anton Ryabchikov <36799941+DedAnton@users.noreply.github.com>
2022-04-26 00:08:24 +0300

    commit result (in development)

--- Awesome Task Exchange System/aTES/Tasks/Program.cs
+++ Awesome Task Exchange System/aTES/Tasks/Program.cs
@@ -1,13 +1,56 @@
+using LinqToDB;
+using Shared;
+using LinqToDB.Mapping;
+using KafkaFlow.TypedHandler;
+using KafkaFlow;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services
+    .AddKafka("broker:9092")
+        .AddConsumer("users", "TasksService")
+            .AddHandler<UserHandler>();
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+app.Services.UseKafka(app.Lifetime);
 
+app.Run($"http://localhost:5002");
 
+public class UserHandler : IMessageHandler<User>
+{
+    public async Task Handle(IMessageContext context, User userMsg)
+    {
+        using var db = new DbTasks();
+        await db.TaskUser.InsertOrUpdateAsync(
+            () => new TaskUser { Id = userMsg.Id, Username = userMsg.Username }, 
+            x => new TaskUser { Id = x.Id, Username = userMsg.Username });
+    }
+}
 
-app.Run();
+public class DbTasks : LinqToDB.Data.DataConnection
+{
+    public DbTasks() : base(@"Server=ANTON-PC\SQLEXPRESS;Database=Tasks;Trusted_Connection=True;Enlist=False;") { }
+
+    public ITable<TesTask> Task => GetTable<TesTask>();
+    public ITable<TaskUser> TaskUser => GetTable<TaskUser>();
+}
+
+[Table(Name = "TaskUser")]
+public class TaskUser
+{
+    [PrimaryKey]
+    public Guid Id { get; set; } = Guid.NewGuid();
+    [Column, NotNull]
+    public string Username { get; set; } = "";
+}
+
+public enum TaskStatus
+{
+    InProgress,
+    Done
+}
--- Awesome Task Exchange System/aTES/oAuthOpernIdConnectMfaMegaAuthorzation/Program.cs
+++ Awesome Task Exchange System/aTES/oAuthOpernIdConnectMfaMegaAuthorzation/Program.cs
@@ -1,13 +1,43 @@
+using LinqToDB;
+using Shared;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services
+    .AddKafka("broker:9092");
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
+app.MapPost("/authorize", (string username, string password) =>
+{
+    using var db = new DbAuthorization();
+    var user = db.Users.FirstOrDefault(x => x.Username == username && x.Password == password);
+    if (user == null)
+    {
+        return Results.NotFound($"Username {username} was not found");
+    }
 
+    return Results.Ok(new { Token = Auth.BuildUserToken(user) });
+});
 
-app.Run();
+app.MapPost("/users", ([Microsoft.AspNetCore.Mvc.FromHeader(Name = "AuthToken")] string token, User newUser) =>
+{
+    using var db = new DbAuthorization();
+    db.Users.Insert(() => newUser);
+
+    return Results.Ok();
+});
+
+app.Run($"http://localhost:5001");
+
+public class DbAuthorization : LinqToDB.Data.DataConnection
+{
+    public DbAuthorization() : base(@"Server=ANTON-PC\SQLEXPRESS;Database=Authorization;Trusted_Connection=True;Enlist=False;") { }
+
+    public ITable<User> Users => GetTable<User>();
+}
